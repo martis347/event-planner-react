0d3f731d180f5e8cce5d8fa457eec76d0e9a0e95
Martynas Kanapinskas <martynas.kanapinskas@gmail.com>
2019-12-16 21:44:49 +0200

    Added workbox

---
+++ public/sw.js
@@ -0,0 +1,8 @@
+/* eslint-disable */
+importScripts(
+  "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js"
+);
+
+if (workbox) {
+  workbox.precaching.precacheAndRoute([]);
+}
